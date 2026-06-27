1cc7c3253b3e118ea15c3534b89a5273f4b1d143
sMv-Developer <smvdeveloper76@gmail.com>
2026-06-27 22:45:36 +0530

    Update server.js
    Added trust proxy to be true for production

--- backEnd/src/server.js
+++ backEnd/src/server.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(cors( { origin: process.env.FRONTEND_URL } ) );
 app.use(express.json());
-// app.set("trust proxy", 1);
+app.set("trust proxy", 1);
 
 // Routes
 app.use("/api", matchRoutes);
